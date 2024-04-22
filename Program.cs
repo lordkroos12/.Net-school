987326d5e071297f44368f1441aecc6de37c6176
lordkroos12 <joroo.23@abv.bg>
2024-04-22 19:58:59 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿namespace Task3
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Enter the number of elements in the array: ");
+			int n = int.Parse(Console.ReadLine());
+
+			int[] originalArray = new int[n];
+
+			for (int i = 0; i < n; i++)
+			{
+				Console.Write($"Enter element {i + 1}: ");
+				originalArray[i] = int.Parse(Console.ReadLine());
+			}
+
+			int[] uniqueArray = RemoveDuplicates(originalArray);
+
+			Console.WriteLine("Array:");
+			PrintArray(originalArray);
+
+			Console.WriteLine("Array without repetitions:");
+			PrintArray(uniqueArray);
+		}
+		static int[] RemoveDuplicates(int[] arr)
+		{
+			bool[] visited = new bool[arr.Length];
+			int uniqueCount = 0;
+
+			for (int i = 0; i < arr.Length; i++)
+			{
+				if (!visited[i])
+				{
+					uniqueCount++;
+
+					for (int j = i + 1; j < arr.Length; j++)
+					{
+						if (arr[i] == arr[j])
+						{
+							visited[j] = true;
+						}
+					}
+				}
+			}
+
+			int[] uniqueArray = new int[uniqueCount];
+			int index = 0;
+
+			for (int i = 0; i < arr.Length; i++)
+			{
+				if (!visited[i])
+				{
+					uniqueArray[index++] = arr[i];
+				}
+			}
+
+			return uniqueArray;
+		}
+
+		static void PrintArray(int[] arr)
+		{
+			foreach (int num in arr)
+			{
+				Console.Write(num + " ");
+			}
+			Console.WriteLine();
+		}
+	}
+}
