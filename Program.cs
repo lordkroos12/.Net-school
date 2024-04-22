638a7de2621c7f28a34167916818df9fc39d74c8
lordkroos12 <joroo.23@abv.bg>
2024-04-22 19:50:04 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Task1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter a and b:");
+			int a = int.Parse(Console.ReadLine());
+			int b = int.Parse(Console.ReadLine());
+
+			Console.WriteLine($"In range [{a}, {b}] with exactly two 'A's in duodecimal representation:");
+
+			for (int i = a; i <= b; i++)
+			{
+				if (CountAInDuodecimal(i) == 2)
+				{
+					Console.WriteLine($"{i} (in decimal)");
+				}
+
+			}
+
+		}
+		static int CountAInDuodecimal(int n)
+		{
+			int count = 0;
+			while (n > 0)
+			{
+				if (n % 12 == 10)
+				{
+					count++;
+				}
+				n /= 12;
+			}
+			return count;
+		}
+	}
+}
