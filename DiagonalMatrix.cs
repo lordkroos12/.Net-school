c9b97fb69a52d99ac38fd404c68f6978eb57ff80
lordkroos12 <joroo.23@abv.bg>
2024-05-15 20:06:18 +0300

    Initial

---
+++ DiagonalMatrix.cs
@@ -0,0 +1,47 @@
+﻿namespace Task4._1
+{
+	public class DiagonalMatrix<T>
+	{
+		private T[] diagMatrix;
+		private int size;
+		public int Size
+		{
+			get { return size; }
+		}
+		public event EventHandler<ElementChangedEventArgs<T>> ElementChanged;
+
+		public DiagonalMatrix(int size)
+		{
+			if (size <= 0)
+				throw new ArgumentException("The length of the array has to be greather than zero!");
+
+			this.size = size;
+			diagMatrix = new T[size];
+		}
+
+		
+
+		public T this[int i, int j]
+		{
+			get
+			{
+				if (i < 0 || i >= size || j < 0 || j >= size)
+					throw new IndexOutOfRangeException("Index not valid.");
+
+				return (i == j) ? diagMatrix[i] : default(T);
+			}
+			set
+			{
+				if (i < 0 || i >= size || j < 0 || j >= size)
+					throw new IndexOutOfRangeException("Index not valid.");
+
+				if (i == j && !Equals(diagMatrix[i], value))
+				{
+					T oldValue = diagMatrix[i];
+					diagMatrix[i] = value;
+					ElementChanged?.Invoke(this, new ElementChangedEventArgs<T>(i, j, oldValue, value));
+				}
+			}
+		}
+	}
+}
---
+++ DiagonalMatrixExtension.cs
@@ -0,0 +1,21 @@
+﻿namespace Task4._1
+{
+	public static class DiagonalMatrixExtensions
+	{
+		public static DiagonalMatrix<T> Add<T>(this DiagonalMatrix<T> matrix1, DiagonalMatrix<T> matrix2, Func<T, T, T> additionFunction)
+		{
+			if (matrix1.Size != matrix2.Size)
+				throw new ArgumentException("Matrices must be of the same size.");
+
+			int size = matrix1.Size;
+			DiagonalMatrix<T> result = new DiagonalMatrix<T>(size);
+
+			for (int i = 0; i < size; i++)
+			{
+				result[i, i] = additionFunction(matrix1[i, i], matrix2[i, i]);
+			}
+
+			return result;
+		}
+	}
+}
