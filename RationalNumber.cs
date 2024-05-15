7a6deba130a00adeb6c784c3d6577a75595a2544
lordkroos12 <joroo.23@abv.bg>
2024-05-15 20:15:05 +0300

    mini fix

--- RationalNumber.cs
+++ RationalNumber.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Task4._2
+﻿namespace Task4._2
 {
 	internal class RationalNumber :IComparable<RationalNumber>
 	{
