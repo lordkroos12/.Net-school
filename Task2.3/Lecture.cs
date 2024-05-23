980a24abc3ad15b0f7b94a66248ede0340d6cd8e
lordkroos12 <joroo.23@abv.bg>
2024-05-23 14:58:59 +0300

    adding all solutions

---
+++ Task2.3/Lecture.cs
@@ -0,0 +1,19 @@
+﻿namespace Task2._3
+{
+	internal class Lecture : BaseLesson
+	{
+		public string Topic { get; set; }
+        public Lecture(string description,string topic): base(description)
+        {
+            this.Topic = topic;
+        }
+        public override object Clone()
+		{
+			return new Lecture(Description,Topic);
+		}
+		public override string ToString()
+		{
+			return $"Lecture: {Topic}";
+		}
+	}
+}
