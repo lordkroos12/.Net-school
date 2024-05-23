d67f8e383fe2277dd9d0d3ff1b3143a8c36f9d7c
lordkroos12 <joroo.23@abv.bg>
2024-05-23 15:06:22 +0300

    Initial

---
+++ Task5.2/Book.cs
@@ -0,0 +1,27 @@
+﻿namespace Task5._2
+{
+	internal class Book
+	{
+		private string title;
+
+		public string Title
+		{
+			get { return title; }
+			set
+			{
+				if (string.IsNullOrWhiteSpace(value))
+					throw new ArgumentNullException("Invalid Title");
+				title = value;
+			}
+		}
+        public DateTime? PublicationDate { get; set; }
+        public List<string> Authors { get; set; }
+        public Book(string title, DateTime pubDate, IEnumerable<string> authors)
+        {
+            this.title = title; 
+			this.PublicationDate = pubDate;
+			this.Authors = (authors ?? new List<string>()).Distinct().ToList();
+        }
+
+    }
+}
